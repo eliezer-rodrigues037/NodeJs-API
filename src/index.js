9249f7001e68fed1474d66d54bc65e82346aab79
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2022-07-26 11:56:22 -0300

    Reinstalled all dependencies.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Importa todas as controllers.
-require('./app/controllers/index')(app);
+require("./app/controllers/index")(app);
 
 app.listen(3000);
